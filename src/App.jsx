dcc0e4ec4aaee3d1be03c8188ad78ccf8cbee9e5
INFERNAL ZERO <153979756+infernalzero123@users.noreply.github.com>
2025-01-16 06:58:46 +0800

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import "react";
 import "./App.css";
 
 export function App() {
-  
-
   return (
     <>
-      
+      {/* Base Containder */}
+      <div className="relative w-screen h-screen bg-zinc-800">
+        {/* Viewport Limiter */}
+        <div className="max-w-[120rem] min-h-screen bg-white mx-auto">
+          {/* Navbar Container */}
+          <div>
+            {/* Navbar Content */}
+            <nav></nav>
+          </div>
+          {/* Section */}
+          <section id="">
+            <h1 className="text-zinc-800 text-3xl">Hello World</h1>
+          </section>
+          {/* Footer */}
+          <footer></footer>
+        </div>
+      </div>
     </>
   );
 }
