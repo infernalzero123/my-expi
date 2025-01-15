4cc496b0a21a694a65b6f79cf255259b62d78da8
INFERNAL ZERO <153979756+infernalzero123@users.noreply.github.com>
2025-01-16 07:24:51 +0800

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import "react";
 import "./App.css";
 
-export function App() {
+export const App = () => {
   return (
     <>
       {/* Base Containder */}
-      <div className="relative w-screen h-screen bg-zinc-800">
+      <div className="relative w-screen min-h-screen bg-zinc-800">
         {/* Viewport Limiter */}
-        <div className="max-w-[120rem] min-h-screen bg-white mx-auto">
+        <div className="relative max-w-[120rem] min-h-screen bg-white mx-auto overflow-hidden">
           {/* Navbar Container */}
           <div>
             {/* Navbar Content */}
             <nav></nav>
           </div>
           {/* Section */}
-          <section id="">
-            <h1 className="text-zinc-800 text-[5rem]">Whereas disregard and contempt for human rights have resulted</h1>
+          <section id="" className="w-full min-h-screen bg-sky-500">
+            <div>
+              <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
+                WHAT I DO
+              </h1>
+            </div>
+          </section>
+          <section id="" className="w-full min-h-screen bg-sky-800">
+            <div>
+              <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
+                section 2
+              </h1>
+            </div>
           </section>
           {/* Footer */}
           <footer></footer>
@@ -23,6 +34,6 @@ export function App() {
       </div>
     </>
   );
-}
+};
 
 export default App;
