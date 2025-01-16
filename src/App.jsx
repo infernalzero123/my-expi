07d2f29e58bcf02fe3f55b45021b07b031daecea
INFERNAL ZERO <153979756+infernalzero123@users.noreply.github.com>
2025-01-16 18:51:14 +0800

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,69 @@
 import "react";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fab, far, fas);
+import {
+  HomeIcon,
+  UserIcon,
+  BookOpenIcon,
+  BuildingLibraryIcon,
+  EnvelopeIcon,
+  BriefcaseIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/outline";
 
 const navItems = [
-  { label: "Home", icon: "fa-solid fa-house" },
-  { label: "About", icon: "fa-solid fa-user" },
-  { label: "Knowledge", icon: "fa-solid fa-book" },
-  { label: "Education", icon: "fa-solid fa-building-columns" },
-  { label: "Contact", icon: "fa-solid fa-envelope" },
-  { label: "Milestone", icon: "fa-solid fa-graduation-cap" },
+  { label: "Home", icon: HomeIcon, href: "home" },
+  { label: "About", icon: UserIcon, href: "about" },
+  { label: "Knowledge", icon: BookOpenIcon, href: "knowledge" },
+  { label: "Projects", icon: BriefcaseIcon, href: "projects" },
+  { label: "Education", icon: BuildingLibraryIcon, href: "education" },
+  { label: "Contact", icon: EnvelopeIcon, href: "contact" },
+  { label: "Milestone", icon: AcademicCapIcon },
 ];
 
 export function App() {
   return (
     <>
       {/* Body Container */}
-      <div className="relative w-screen min-h-screen lg:pl-60 overflow-hidden m-0 mt-16 lg:mt-0 p-0 ">
+      <div className="w-screen min-h-screen overflow-hidden m-0 mt-16 p-0 lg:mt-0">
         {/* Navbar Container */}
-        <div className="fixed top-0 left-0 w-full min-h-20 lg:w-60 lg:h-screen overflow-hidden bg-zinc-800 shadow-lg z-50">
+        <div className="fixed top-0 left-0 w-full min-h-20 lg:w-56 lg:h-screen overflow-hidden bg-zinc-800 shadow-lg z-50">
           {/* Navbar Content */}
-          <nav className="w-full min-h-16 lg:w-60 lg:h-screen">
-            <div className="hidden lg:block w-60 h-20"></div>
-            <ul className="hidden lg:block flex-row lg:flex-col space-y-4 px-6 py-3 transition-all duration-0">
+          <nav>
+            <div className="hidden lg:block w-full h-20 bg-lime-400"></div>
+            <ul className="relative hidden lg:block flex-row lg:flex-col space-y-3 pl-8 px-5">
+              <div className="absolute top-0 left-4 w-[2px] h-full bg-zinc-100 rounded-full">
+                <div className="absolute top-0 left-0 ml-[-7px] w-4 h-4 bg-zinc-800 border-2 border-zinc-100 rounded-full">
+                  {/* Active Link Pointer */}
+                </div>
+              </div>
               {navItems.map((item, index) => (
                 <li
                   key={index}
-                  className="group flex items-center space-x-5 text-zinc-0 transition-all duration-0 hover:text-blue-500"
+                  className="group w-11 overflow-hidden flex items-center text-zinc-100 bg-zinc-7000 transition-all duration-100 hover:bg-zinc-100/10 space-x-5 py-2 pl-2 pr-4 rounded-md"
                 >
-                  {/* Icon Container with Fixed Size */}
-                  <div className="w-8 h-8 mr-1 flex justify-center items-center group-hover:text-blue-500 transition-all duration-0 bg-lime-500">
-                    <FontAwesomeIcon
-                      icon={item.icon.split(" ")}
-                      className="text-2xl transition-all duration-0"
-                    />
-                  </div>
-                  {/* Label */}
-                  <span className="whitespace-nowrap transition-all duration-0 group-hover:text-blue-500">
-                    {item.label}
-                  </span>
+                  {/* Conditionally render anchor tag for items with href */}
+                  {item.href ? (
+                    <a
+                      href={`#${item.href}`}
+                      className="flex items-center space-x-6 w-full"
+                    >
+                      <div className="w-8 h-8 flex justify-center items-center">
+                        <item.icon className="w-7 h-7 transition-all duration-0" />
+                      </div>
+                      <span className="whitespace-nowrap transition-all duration-100">
+                        {item.label}
+                      </span>
+                    </a>
+                  ) : (
+                    <div className="flex items-center space-x-6 w-full">
+                      <div className="w-8 h-8 mr-1 flex justify-center items-center">
+                        <item.icon className="w-7 h-7 transition-all duration-0" />
+                      </div>
+                      <span className="whitespace-nowrap transition-all duration-100">
+                        {item.label}
+                      </span>
+                    </div>
+                  )}
                 </li>
               ))}
             </ul>
@@ -50,18 +71,18 @@ export function App() {
         </div>
         {/* Section */}
         <section
-          id=""
-          className="w-full min-h-screen bg-sky-500 overflow-hidden box-border m-0 p-0"
+          id="home"
+          className="w-full min-h-screen bg-sky-500 overflow-hidden box-border m-0 p-0 lg:pl-60"
         >
           <div>
             <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
-              WHAT I DO
+              SECTION 1
             </h1>
           </div>
         </section>
         <section
-          id=""
-          className="w-full min-h-screen bg-sky-700 overflow-hidden box-border m-0 p-0"
+          id="about"
+          className="w-full min-h-screen overflow-hidden box-border m-0 p-0 lg:pl-60"
         >
           <div>
             <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
--- src/TempBack.jsx
+++ src/TempBack.jsx
@@ -1,52 +1,72 @@
 import "react";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-library.add(fab, far, fas);
+import {
+  HomeIcon,
+  UserIcon,
+  BookOpenIcon,
+  BuildingLibraryIcon,
+  EnvelopeIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/outline";
 
-
-
+const navItems = [
+  { label: "Home", icon: HomeIcon },
+  { label: "About", icon: UserIcon },
+  { label: "Knowledge", icon: BookOpenIcon },
+  { label: "Education", icon: BuildingLibraryIcon },
+  { label: "Contact", icon: EnvelopeIcon },
+  { label: "Milestone", icon: AcademicCapIcon },
+];
 
 export function App() {
   return (
     <>
-      {/* Base Container */}
-      <div className="relative w-screen min-h-screen bg-zinc-800 overflow-hidden m-0 p-0">
-        {/* Viewport Limiter */}
-        <div className="relative max-w-[120rem] min-h-screen bg-white mx-auto overflow-hidden m-0 p-0">
-          {/* Navbar Container */}
-          <div>
-            {/* Navbar Content */}
-            <nav></nav>
-          </div>
-          {/* Section */}
-          <section
-            id=""
-            className="w-full min-h-screen bg-sky-500 overflow-hidden box-border m-0 p-0"
-          >
-            <div>
-              <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
-                WHAT I DO
-                <FontAwesomeIcon icon="fa-solid fa-house" size="5x" />
-              </h1>
-            </div>
-          </section>
-          <section
-            id=""
-            className="w-full min-h-screen bg-sky-700 overflow-hidden box-border m-0 p-0"
-          >
-            <div>
-              <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
-                SECTION 2
-              </h1>
-            </div>
-          </section>
-          {/* Footer */}
-          <footer></footer>
+      {/* Body Container */}
+      <div className="relative w-screen min-h-screen lg:pl-60 overflow-hidden m-0 mt-16 lg:mt-0 p-0 ">
+        {/* Navbar Container */}
+        <div className="fixed top-0 left-0 w-full min-h-20 lg:w-20 lg:h-screen overflow-hidden bg-zinc-800 shadow-lg z-50">
+          {/* Navbar Content */}
+          <ul className="hidden lg:block flex-row lg:flex-col space-y-2 px-6 py-3">
+            {navItems.map((item, index) => (
+              <li
+                key={index}
+                className="group flex items-center space-x-5 py-2 text-zinc-100 bg-zinc-700 transition-all duration-100 hover:text-blue-500"
+              >
+                {/* Icon Container with Fixed Size */}
+                <div className="w-8 h-8 mr-1 flex justify-center items-center">
+                  <item.icon className="h-6 w-6 transition-all duration-0 group-hover:text-blue-500" />
+                </div>
+                {/* Label */}
+                <span className="whitespace-nowrap transition-all duration-100 group-hover:text-blue-500">
+                  {item.label}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
+        {/* Section */}
+        <section
+          id=""
+          className="w-full min-h-screen bg-sky-500 overflow-hidden box-border m-0 p-0"
+        >
+          <div>
+            <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
+              WHAT I DO
+            </h1>
+          </div>
+        </section>
+        <section
+          id=""
+          className="w-full min-h-screen bg-sky-700 overflow-hidden box-border m-0 p-0"
+        >
+          <div>
+            <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
+              SECTION 2
+            </h1>
+          </div>
+        </section>
+        {/* Footer */}
+        <footer></footer>
       </div>
     </>
   );
