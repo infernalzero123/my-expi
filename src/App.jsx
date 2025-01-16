5489b88f01dcdb822ad34a8d167b1d7535c812bb
INFERNAL ZERO <153979756+infernalzero123@users.noreply.github.com>
2025-01-16 08:10:19 +0800

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import "react";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fab, far, fas);
 
-export const App = () => {
+
+
+
+export function App() {
   return (
     <>
-      {/* Base Containder */}
-      <div className="relative w-screen min-h-screen bg-zinc-800">
+      {/* Base Container */}
+      <div className="relative w-screen min-h-screen bg-zinc-800 overflow-hidden m-0 p-0">
         {/* Viewport Limiter */}
-        <div className="relative max-w-[120rem] min-h-screen bg-white mx-auto overflow-hidden">
+        <div className="relative max-w-[120rem] min-h-screen bg-white mx-auto overflow-hidden m-0 p-0">
           {/* Navbar Container */}
           <div>
             {/* Navbar Content */}
             <nav></nav>
           </div>
           {/* Section */}
-          <section id="" className="w-full min-h-screen bg-sky-500">
+          <section
+            id=""
+            className="w-full min-h-screen bg-sky-500 overflow-hidden box-border m-0 p-0"
+          >
             <div>
               <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
-                WHAT I DOS
+                WHAT I DO
+                <FontAwesomeIcon icon="fa-solid fa-house" size="5x" />
               </h1>
             </div>
           </section>
-          <section id="" className="w-full min-h-screen bg-sky-800">
+          <section
+            id=""
+            className="w-full min-h-screen bg-sky-700 overflow-hidden box-border m-0 p-0"
+          >
             <div>
               <h1 className="text-zinc-800 text-[2rem] font-mono font-semibold">
-                section 2
+                SECTION 2
               </h1>
             </div>
           </section>
@@ -34,6 +50,6 @@ export const App = () => {
       </div>
     </>
   );
-};
+}
 
 export default App;
